8c9ff49911ccbe8a6b58a8e30c4e074a20fcce8a
sk <seyed.ketabchi@gmail.com>
2017-02-17 11:43:34 -0800

    BlackFriday-commit

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App by Seyed Ketabchi on this date: </title>
+        <title>Glidewell PHP App ci/cd demo </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple PHP App - Changed</h1>
-                <h2>Congratulations Seyed! Happy Coding :) </h2>
+                <h2>Your changes when you were Coding... </h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
