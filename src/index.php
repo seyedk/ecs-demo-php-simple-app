e607468defa6ef55b7f88d4e3a3625d23387460f
sk <seyed.ketabchi@gmail.com>
2017-04-20 10:14:59 -0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Beckman .NET App ci/cd demo </title>
+        <title>Beckman Culter's .NET App ci/cd demo </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
